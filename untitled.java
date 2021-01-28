b7e7bf558714226fd5fa168227a9ed5a0d440a55
David Boylan <59584695+David-Boylan@users.noreply.github.com>
2021-01-28 17:12:46 -0600

    Add files via upload

--- untitled.java
+++ untitled.java
@@ -21,15 +21,15 @@ public class untitled{
             int gamble7 = rand.nextInt(10);
             int gamble8 = rand.nextInt(10);
             int gamble9 = rand.nextInt(10);
-            System.out.print(gamble1);
-            System.out.print(gamble2);
-            System.out.println(gamble3);
-            System.out.print(gamble4);
-            System.out.print(gamble5);
-            System.out.println(gamble6);
-            System.out.print(gamble7);
-            System.out.print(gamble8);
-            System.out.print(gamble9);
+            System.out.print(gamble1 + "  ");
+            System.out.print(gamble2 + "  ");
+            System.out.println(gamble3 + "  ");
+            System.out.print(gamble4 + "  ");
+            System.out.print(gamble5 + "  ");
+            System.out.println(gamble6 + "  ");
+            System.out.print(gamble7 + "  ");
+            System.out.print(gamble8 + "  ");
+            System.out.print(gamble9 + "  ");
             if ((gamble1 == gamble2 && gamble2 == gamble3)||
             (gamble1 == gamble5 && gamble5 == gamble9)||
             (gamble1 == gamble4 && gamble4 == gamble7)||
